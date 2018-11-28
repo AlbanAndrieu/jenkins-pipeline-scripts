e51e72d273a0a8970d45cfa5f51e5b696fd1da2c
Andrieu, Alban <alban.andrieu@misys.com>
2018-11-28 15:10:20 +0100

    Debug curl not found

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ export reverse_exclamation='\u00A1'
 # Assuming "anonymous read access" has been enabled on your Jenkins instance.
 # JENKINS_URL=[root URL of Jenkins master]
 JENKINS_URL="https://home.nabla.mobi:8381/"
+which curl || true
 # JENKINS_CRUMB is needed if your Jenkins master has CRSF protection enabled as it should
 JENKINS_CRUMB=`curl "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"`
 curl -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<Jenkinsfile" $JENKINS_URL/pipeline-model-converter/validate
