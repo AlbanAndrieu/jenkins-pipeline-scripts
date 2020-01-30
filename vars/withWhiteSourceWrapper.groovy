0a7beda426a9a9c41226e53d00638b17dd4fcbcd
Andrieu, Alban <alban.andrieu@finastra.com>
2020-01-30 14:00:12 +0100

    Update withsource token

--- vars/withWhiteSourceWrapper.groovy
+++ vars/withWhiteSourceWrapper.groovy
@@ -15,11 +15,13 @@ def call(Map vars, Closure body=null) {
   def RELEASE = vars.get("RELEASE", env.RELEASE ?: false).toBoolean()
   def RELEASE_VERSION = vars.get("RELEASE_VERSION", env.RELEASE_VERSION ?: null)
 
-  String WHITESOURCE_TOKEN = vars.get("WHITESOURCE_TOKEN", "TODO").trim()
+  String WHITESOURCE_TOKEN = vars.get("WHITESOURCE_TOKEN", "27651afa-9e00-4c57-9f0e-7df9358e6750").trim()
 
   vars.productVersion = vars.get("productVersion", "").trim()
-  vars.product = vars.get("product", "FusionRisk").trim()
+  vars.product = vars.get("product", "nabla").trim()
   vars.projectToken = vars.get("projectToken", "").trim()
+  #productToken 2dcf60630aca4d3fb29fe59ad731d488747f2f6b0ba04b8b8664ae3629a1c3ae
+  vars.jobUserKey = vars.get("jobUserKey", "cf5b762ee7ab4f2cb9fdab9728e59c2a3ccc2d77cb9b4718986dfe90fac671bb").trim()
 
   if (vars.projectToken?.trim()) {
       vars.requesterEmail = vars.get("requesterEmail", 'alban.andrieu@free.fr').trim()
@@ -37,7 +39,7 @@ def call(Map vars, Closure body=null) {
             vars.productVersion = "${RELEASE_VERSION}"
         }
 
-        whitesource jobApiToken: WHITESOURCE_TOKEN, jobCheckPolicies: 'global', jobForceUpdate: 'global', jobUserKey: '', libExcludes: '', libIncludes: '', product: vars.product, productVersion: vars.projectToken, projectToken: vars.projectToken, requesterEmail: vars.requesterEmail
+        whitesource jobApiToken: WHITESOURCE_TOKEN, jobCheckPolicies: 'global', jobForceUpdate: 'global', jobUserKey: vars.jobUserKey, libExcludes: '', libIncludes: '', product: vars.product, productVersion: vars.productVersion, projectToken: vars.projectToken, requesterEmail: vars.requesterEmail
 
         if (body) { body() }
 
