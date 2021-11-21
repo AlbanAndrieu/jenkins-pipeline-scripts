d89868d5553acabf4b7df0c190c0e86a9000a8d6
Andrieu, Alban <alban.andrieu@finastra.com>
2021-11-21 19:12:08 +0100

    Update github action

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 # shellcheck source=/dev/null
-source "${WORKING_DIR}/step-0-color.sh"
+source "${WORKING_DIR}/scripts/step-0-color.sh"
 
 export REPO_TAG=${REPO_TAG:-"1.1.0"}
 
--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ set -eu
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 # shellcheck source=/dev/null
-source "${WORKING_DIR}/step-0-color.sh"
+source "${WORKING_DIR}/scripts/step-0-color.sh"
 
 #package-lock.json yarn.lock
 rm -Rf .scannerwork/ .gradle/ npm/ .node_cache/ .node_tmp/  .tmp/ .bower/ bower_components/ node node_modules/ .sass-cache/ target/ target-eclipse/ build/ phantomas/ dist/ docs/groovydocs/ docs/js/ docs/partials/ site/ coverage/
@@ -12,5 +12,7 @@ rm -Rf .scannerwork/ .gradle/ npm/ .node_cache/ .node_tmp/  .tmp/ .bower/ bower_
 #dist/bower_components/ dist/fonts/
 
 rm -f checkstyle.xml
+
+echo -e "${green} Cleaning DONE ${NC}"
 
 exit 0
